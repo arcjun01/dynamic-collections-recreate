bef4ddc9299f4ce81048fbe61bc112bca31680fb
Lizock <khyelyza@gmail.com>
2025-10-14 14:10:14 -0700

    Added App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Content from "./components/Content"
+import data from "./players"
+
+export default function App() {
+  const contentElements = data.map((content) => {
+    return 
+    (<Content 
+      key={content.id}
+      content={content}
+    />)
+  })
+}
+
+return (
+  <>
+    <Header />
+    <main>
+      {contentElements}
+    </main>
+    <Footer/>
+  </>
+)
