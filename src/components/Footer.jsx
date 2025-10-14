acccf2464b2d8517c2d138e44238aca498de1b25
Lizock <khyelyza@gmail.com>
2025-10-14 13:43:31 -0700

    Created a Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <footer classname="footer">
+            <small>© 2025 Modern Mariners</small>
+        </footer>
+    )
+
+}
