b227cf888c442dd811145d209f3d7f26d07193e5
arcjun01 <arciaga.juno@student.greenriver.edu>
2025-10-14 14:03:46 -0700

    Implemented Content and added images

---
+++ src/components/Content.jsx
@@ -0,0 +1,15 @@
+export default function Content(props) {
+
+    const { img, name, position, jersey, teams } = props.content
+
+    return (
+        <div className="playerCard">
+            <img src={img}></img>
+            <h1>Name: {name}</h1>
+            <h2>Position: {position}</h2>
+            <h2>Number: {jersey}</h2>
+            <h2>Teams: {teams}</h2>            
+    
+        </div>
+    )
+}
