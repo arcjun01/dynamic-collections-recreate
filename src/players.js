b05d5d6ce2b325f09ae9eef764934c0fa389f9f5
arcjun01 <arciaga.juno@student.greenriver.edu>
2025-10-14 13:33:20 -0700

    Created Component files

---
+++ src/players.js
@@ -0,0 +1,135 @@
+export const players = [
+  // --- Starting 9 ---
+  {
+    "name": "Julio Rodríguez",
+    "img": "../images/julio-r.png",
+    "position": "CF",
+    "jersey": "44",
+    "birthdate": "12/29/2000",
+    "age": "24",
+    "stat": "AVG: .267",
+    "teams": ["DSL Mariners", "Arkansas Travelers"]
+  },
+  {
+    "name": "J.P. Crawford",
+    "img": "../images/jp-c.png",
+    "position": "SS",
+    "jersey": "3",
+    "birthdate": "01/11/1995",
+    "age": "30",
+    "stat": "AVG: .265",
+    "teams": ["Philadelphia Phillies", "Lakewood BlueClaws"]
+  },
+  {
+    "name": "Cal Raleigh",
+    "img": "../images/cal-r.png",
+    "position": "C",
+    "jersey": "29",
+    "birthdate": "11/26/1996",
+    "age": "28",
+    "stat": "AVG: .247",
+    "teams": ["Florida State University", "Tacoma Rainiers"]
+  },
+  {
+    "name": "Jorge Polanco",
+    "img": "../images/jorge-p.png",
+    "position": "INF / DH",
+    "jersey": "7",
+    "birthdate": "07/05/1993",
+    "age": "32",
+    "stat": "AVG: .265",
+    "teams": ["Minnesota Twins", "Fort Myers Miracle"]
+  },
+  {
+    "name": "Eugenio Suárez",
+    "img": "../images/eugenio-s.png",
+    "position": "3B / INF",
+    "jersey": "28",
+    "birthdate": "07/18/1991",
+    "age": "34",
+    "stat": "AVG: .246",
+    "teams": ["Cincinnati Reds", "Detroit Tigers"]
+  },
+  {
+    "name": "Randy Arozarena",
+    "img": "../images/randy-a.png",
+    "position": "OF",
+    "jersey": "56",
+    "birthdate": "02/28/1995",
+    "age": "30",
+    "stat": "AVG: .238",
+    "teams": ["Tampa Bay Rays", "St. Louis Cardinals"]
+  },
+  {
+    "name": "Dominic Canzone",
+    "img": "../images/dominic-c.png",
+    "position": "OF",
+    "jersey": "5",
+    "birthdate": "08/16/1997",
+    "age": "28",
+    "stat": "AVG: .254",
+    "teams": ["Arizona Diamondbacks", "Ohio State University"]
+  },
+  {
+    "name": "Victor Robles",
+    "img": "../images/victor-r.png",
+    "position": "OF",
+    "jersey": "14",
+    "birthdate": "05/19/1997",
+    "age": "28",
+    "stat": "AVG: .227",
+    "teams": ["Washington Nationals", "Hagerstown Suns"]
+  },
+  {
+    "name": "Josh Naylor",
+    "img": "../images/josh-n.png",
+    "position": "1B",
+    "jersey": "22",
+    "birthdate": "06/22/1997",
+    "age": "28",
+    "stat": "AVG: .271",
+    "teams": ["Cleveland Guardians", "San Diego Padres"]
+  },
+
+  // --- Pitchers ---
+  {
+    "name": "Luis Castillo",
+    "img": "../images/luis-c.png",
+    "position": "P",
+    "jersey": "58",
+    "birthdate": "12/12/1992",
+    "age": "32",
+    "stat": "ERA: 3.62",
+    "teams": ["Cincinnati Reds", "Miami Marlins"]
+  },
+  {
+    "name": "George Kirby",
+    "img": "../images/george-k.png",
+    "position": "P",
+    "jersey": "68",
+    "birthdate": "02/04/1998",
+    "age": "27",
+    "stat": "ERA: 3.45",
+    "teams": ["Elon University", "Everett AquaSox"]
+  },
+  {
+    "name": "Bryan Woo",
+    "img": "../images/bryan-w.png",
+    "position": "P",
+    "jersey": "22",
+    "birthdate": "01/30/2000",
+    "age": "25",
+    "stat": "ERA: 3.32",
+    "teams": ["Cal Poly San Luis Obispo", "Arkansas Travelers"]
+  },
+  {
+    "name": "Andrés Muñoz",
+    "img": "../images/andres-m.png",
+    "position": "P",
+    "jersey": "75",
+    "birthdate": "01/16/1999",
+    "age": "26",
+    "stat": "ERA: 2.81",
+    "teams": ["San Diego Padres", "El Paso Chihuahuas"]
+  }
+];
